bd8be1c0da16eb71d9149a1dbed8b8c03d7311fe
drkitty <bumblingkitty@gmail.com>
2014-03-01 16:27:34 -0800

    Disable submit button when boxes aren't checked

---
+++ static/js/terms.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $('#agree1,#agree2').on('click', function() {
+        $("button#submit").attr("disabled",
+            !($('#agree1')[0].checked && $('#agree2')[0].checked));
+    });
+
+    $("button#submit").on('click', function(e) {
+        e.preventDefault();
+        location = '/devices';
+    });
+});
